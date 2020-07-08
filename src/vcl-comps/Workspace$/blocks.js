a2e9d7739ce48c8b1016e58d4141e9507fc0fe37
Ralph Kazemier <ralph.kazemier@gmail.com>
2020-07-08 14:32:43 -0500

    updating

--- src/vcl-comps/Workspace$/blocks.js
+++ src/vcl-comps/Workspace$/blocks.js
@@ -15,7 +15,7 @@ $([], {
 			// "Workspaces/cavalion.org/cavalion-devtools/src/cavalion-blocks/devtools",
 			// "Workspaces/cavalion.org/cavalion-ide/src/cavalion-blocks/ide",
 			// "Workspaces/veldapps.com/veldapps-vo/src/cavalion-blocks;veldapps",
-			"Library/cavalion-blocks/console;;Folder",
+			// "Library/cavalion-blocks/console;;Folder",
 			"Library/cavalion-blocks/devtools;;Folder",
 			"Library/cavalion-blocks/docs;;Folder",
 			"Library/cavalion-blocks/ide;;Folder",
@@ -32,22 +32,22 @@ $([], {
 		var fs = this.down("#tree < #fs");
 		var NavigatorNode = fs.constructor;
 
-		var node = new NavigatorNode({
-			vars: { 
-				resource: { 
-					uri: "pouchdb://va_objects",
-					name: "va-objects", 
-					type: "Folder"
-				}
-			},
-			owner: this,
-			parent: this.scope().databases,
-			expandable: true,
-			onNodesNeeded: function() {
-				var fs = this.up("devtools/Workspace<>").down("#navigator #fs");
-				return fs._onChildNodesNeeded.apply(fs, arguments);
-			}
-		});
+		// var node = new NavigatorNode({
+		// 	vars: { 
+		// 		resource: { 
+		// 			uri: "pouchdb://va_objects",
+		// 			name: "va-objects", 
+		// 			type: "Folder"
+		// 		}
+		// 	},
+		// 	owner: this,
+		// 	parent: this.scope().databases,
+		// 	expandable: true,
+		// 	onNodesNeeded: function() {
+		// 		var fs = this.up("devtools/Workspace<>").down("#navigator #fs");
+		// 		return fs._onChildNodesNeeded.apply(fs, arguments);
+		// 	}
+		// });
 
 		// must have a Resources implementation, take it from fs		
 		this.setVar("Resources", fs.getVar("Resources"));
@@ -82,48 +82,48 @@ $([], {
 		return this.inherited(arguments);
 	}
 },  [
-	$i("navigator", [
-		$i("tree", [
-			$("vcl/ui/Node", {
-				text: "DROPBOX",
-				classes: "folder seperator",
-				expandable: true,
-				// index: 0,
-				onNodesNeeded: function(parent) {
-					parent = parent || this;
+	// $i("navigator", [
+	// 	$i("tree", [
+	// 		$("vcl/ui/Node", {
+	// 			text: "DROPBOX",
+	// 			classes: "folder seperator",
+	// 			expandable: true,
+	// 			// index: 0,
+	// 			onNodesNeeded: function(parent) {
+	// 				parent = parent || this;
 			
-					var owner = this;
-					var dbx = this.vars(["dbx", true]);
-					return dbx.filesListFolder({path: parent.vars("path") || ""})
-						.then(function(res) {
-							res.entries.sort(function(i1, i2) {
-								return i1.name < i2.name ? -1 : 1;
-							}).forEach(function(entry) {
-								var folder = entry['.tag'] === "folder";
-								(new Node_(owner)).setProperties({
-									parent: parent,
-									expandable: folder,
-									classes: folder ? "folder" : "file",
-									text: entry.name || entry.path_display
-								}).vars("path", entry.path_display);
-							});
-						});
-				}
-			}),
-			$("vcl/ui/Node", "databases", {
-				text: "Databases",
-				// visible: false,
-				classes: "_root-invisible folder seperator",
-				expanded: true,
-				// onLoad: function() {
-				// 	var fs = this.up("devtools/Workspace<>").down("#navigator #fs");
-				// 	this.setParent(fs);
-				// 	this.show();
-				// },
-				onNodesNeeded: function() {}
-			})
-		])	
-	])
+	// 				var owner = this;
+	// 				var dbx = this.vars(["dbx", true]);
+	// 				return dbx.filesListFolder({path: parent.vars("path") || ""})
+	// 					.then(function(res) {
+	// 						res.entries.sort(function(i1, i2) {
+	// 							return i1.name < i2.name ? -1 : 1;
+	// 						}).forEach(function(entry) {
+	// 							var folder = entry['.tag'] === "folder";
+	// 							(new Node_(owner)).setProperties({
+	// 								parent: parent,
+	// 								expandable: folder,
+	// 								classes: folder ? "folder" : "file",
+	// 								text: entry.name || entry.path_display
+	// 							}).vars("path", entry.path_display);
+	// 						});
+	// 					});
+	// 			}
+	// 		}),
+	// 		$("vcl/ui/Node", "databases", {
+	// 			text: "Databases",
+	// 			// visible: false,
+	// 			classes: "_root-invisible folder seperator",
+	// 			expanded: true,
+	// 			// onLoad: function() {
+	// 			// 	var fs = this.up("devtools/Workspace<>").down("#navigator #fs");
+	// 			// 	this.setParent(fs);
+	// 			// 	this.show();
+	// 			// },
+	// 			onNodesNeeded: function() {}
+	// 		})
+	// 	])	
+	// ])
 ]);
 
 
