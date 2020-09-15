ce40a2b2fc9146ac198b10156f3b26c56323938a
Ralph Kazemier <ralph.kazemier@gmail.com>
2020-09-14 19:16:10 -0500

    enhancing Console, need to "centralize" often-use-code in IPFS? make it better and easily accessible

--- src/vcl-comps/Console.js
+++ src/vcl-comps/Console.js
@@ -1,5 +1,25 @@
-"use js";
+"use devtools/Resources";
+
+var Resources = require("devtools/Resources");
 
 ["vcl/ui/Form", (""), { activeControl: "console" }, [
-	["vcl/ui/Console", ("console"), { classes: "no-time" }]
+	["vcl/ui/Console", ("console"), { 
+		classes: "no-time",
+        onEvaluate(expr) {
+            var scope = this.scope();
+            var app = this.app(), me = this;
+            var ws = this.up(":root");
+            var ace = ws && ws.down("devtools/Editor<>:root < vcl/ui/Ace:visible");
+            var ed = ace ? ace.getEditor() : null;
+            // var paths = window.require.s.contexts._.config.paths;
+            
+            var pr = this.print.bind(this);
+            var open = (uri, opts) => me.bubble("openform", js.mixIn(js.mixIn(opts || {}), {uri: uri}));
+            var ls = (path) => Resources.list(path || "/");
+
+            // return window["ev"+(pr?"al":"")].apply(this, [expr]); // don't want no warnings (just calling eval)
+            return eval(expr);
+        }
+	
+	}]
 ]];
