cb5bc5036bd75e08d2bfb2aaf093ed2c51d71c44
Ralph Kazemier <ralph.kazemier@gmail.com>
2020-10-01 10:11:18 -0500

    first commit

---
+++ src/devtools/Resources-dropped.js
@@ -0,0 +1,59 @@
+define(function(require) {
+
+	var Hash = require("util/Hash");
+	
+	var allDroppedItems = () => require("vcl/Application").get()
+			.qsa("devtools/DragDropHandler<>")
+			.map(_ => _.vars("dropped")).flat()
+			.map(_ => _.items)
+			.flat();
+
+	return {
+		index: function(uris) {
+			return {
+				"": allDroppedItems().map((item, index) => ({
+					path: js.sf("%s", index),
+					name: js.get("fileValue.name", item),
+					uri: js.sf("%s/%s", index, js.get("fileValue.name", item)),
+					lastModified: js.get("fileValue.lastModified", item),
+					size: js.get("fileValue.size", item),
+					type: "File"
+				}))
+			};
+		},
+		list: function(path) {
+			return Promise.resolve(
+				allDroppedItems().map((item, index) => ({
+					path: js.sf("%s", index),
+					name: js.get("fileValue.name", item),
+					uri: js.sf("%s/%s", index, js.get("fileValue.name", item)),
+					lastModified: js.get("fileValue.lastModified", item),
+					size: js.get("fileValue.size", item),
+					type: "File"
+				})));
+		},
+		get: function(uri) {
+			var index = parseInt(uri.split("/").shift(), 10);
+			var item = allDroppedItems()[index];
+			
+			return Promise.resolve({
+				path: js.up(uri),
+				name: uri.split("/").pop(),
+				uri: uri,
+				type: "File",
+				text: item.readerResult
+			});
+		},
+		
+	/** not implemented below here */
+		create: function(path, resource) {
+		},
+		'delete': function(path) {
+		},
+		update: function(path, resource) {
+		},
+		link: function(path) {
+		}
+
+	};
+});
