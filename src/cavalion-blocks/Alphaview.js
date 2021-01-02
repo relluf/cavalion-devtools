18be7d5db9164856689275fc75bfdb82bb1c4673
Ralph Kazemier <ralph.kazemier@gmail.com>
2021-01-02 12:31:18 -0600

    adds csv export

--- src/cavalion-blocks/Alphaview.js
+++ src/cavalion-blocks/Alphaview.js
@@ -458,6 +458,33 @@ var Factories = {
 			}
 		}
 	}],
+	["Executable", ("csv"), {
+		on(evt) {
+			var selection = this.ud("#list").getSelection(true);
+			var filtered = this.ud("#q").getValue().length > 0;
+			if(selection.length === 0) {
+				selection = this.ud("#array")[filtered ? '_arr' : '_array'];
+			}
+
+var nameOf = (_) => _ instanceof Array ? js.sf("%d items", _.length) : js.nameOf(_);
+
+			var ws = this.up("devtools/Workspace<>:root");
+			var tab = ws.down("#editor-needed").execute({
+				resource: { uri: "Resource-" + Date.now() + ".csv" },
+				selected: true
+			});
+			
+			var header = [];
+			selection.forEach(_ => Object.keys(_).forEach(key => !header.includes(key) ? header.push(key) : []));
+			
+			// this.print("remove", header.splice(0, 1)); // ? (1*)
+			
+			selection = selection.map(_ => js.sf("\"%s\"", header.map(h => _[h] !== undefined ? nameOf(_[h]) : "").join("\",\"")));
+			tab.once("resource-loaded", function() {
+				tab.down("#ace").setValue([header].concat(selection).join("\n"));
+			});
+		}
+	}],
 	
 	["Array", ("array"), { 
 		onFilterObject(obj) {
@@ -487,7 +514,8 @@ var Factories = {
 				}
 			}],
 			["Button", { action: "map" }],
-			["Button", { action: "map-xy" }]
+			["Button", { action: "map-xy" }],
+			["Button", { action: "csv" }]
 		]],
 		["Input", ("q"), { 
 			placeholder: "Filter", 
