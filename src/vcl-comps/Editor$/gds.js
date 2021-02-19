8436a4df0b759f5a78e7b8f3bc8bd9aa0cea0bba
Ralph Kazemier <ralph.kazemier@gmail.com>
2021-02-18 22:41:07 -0600

    1st commit #VA-20201218-3

---
+++ src/vcl-comps/Editor$/gds.js
@@ -0,0 +1,118 @@
+"use lib/bower_components/papaparse/papaparse, amcharts, amcharts.serial, amcharts.xy";
+
+var Parser = require("lib/bower_components/papaparse/papaparse");
+
+["", {}, [
+    [("#ace"), { 
+    	align: "left", width: 475, action: "toggle-source",
+    	executesAction: "none",
+        onChange() {
+        	this.setTimeout("render", () => this.ud("#render").execute(), 750);
+        }
+    }],
+
+    ["vcl/Action", ("toggle-source"), {
+        hotkey: "Shift+MetaCtrl+S",
+        selected: "state", visible: "state", 
+        state: true,
+        onLoad() {
+    		this.up().readStorage("source-visible", (visible) => {
+    			if(typeof visible === "boolean") {
+    				this.setState(visible);
+    			} else if(visible === undefined && this.vars(["resource.uri"]).split("/").pop() === ".md") {
+    				this.setState(false);
+    			}
+    		});
+        },
+        onExecute() {
+        	var state = !this.getState();
+        	this.setState(state);
+        	this.up().writeStorage("source-visible", state);
+        	if(!state) {
+        		
+        	}
+        }
+    }],
+    ["vcl/Action", ("render"), {
+    	on() {
+    		var ace = this.ud("#ace");
+			var lines = ace.getLines().filter(_ => _.split(",").length > 5);
+			
+			var options = this.vars(["options"]) || {
+				// delimiter: "",	// auto-detect
+				// newline: "",	// auto-detect
+				// quoteChar: '"',
+				// escapeChar: '"',
+				// header: false,
+				// dynamicTyping: false,
+				// preview: 0,
+				// encoding: "",
+				// worker: false,
+				// comments: false,
+				// step: undefined,
+				// complete: undefined,
+				// error: undefined,
+				// download: false,
+				// skipEmptyLines: false,
+				// chunk: undefined,
+				// fastMode: undefined,
+				// beforeFirstChunk: undefined,
+				// withCredentials: undefined
+			};
+			var arr = Parser.parse(lines.join("\n"), options).data;
+			var headers = arr.shift();
+			var parseValue = (value) => isNaN(value.replace(",", ".")) ? value : parseFloat(value.replace(",", "."));
+			
+			var vf = this.ud("#valueField"), vf_kPa = this.ud("#valueField_kPa"), cf = this.ud("#categoryField");
+			cf.setOptions(headers.slice(0, 3));
+			vf.setOptions(headers.slice(3)
+				.filter(_ => !_.includes("Undefined"))
+				.filter(_ => !_.includes("Temperature"))
+				.filter(_ => !_.includes("Pressure")));
+			vf_kPa.setOptions(vf._options);
+			
+			vf.setValue(vf._options[1]);
+			vf_kPa.setValue(vf._options[4]);
+			cf.setValue(cf._options[1]);
+			
+			arr = arr.map(values => {
+				var obj = {};
+				headers.forEach((key, index) => obj[key] = parseValue(values[index]));
+				return obj;
+			});
+			
+			this.ud("#array").setArray(arr);
+			this.up("vcl/ui/Tab").emit("resource-rendered", [{sender: this, data: arr}]);
+    	}
+    }],
+	
+	["vcl/data/Array", ("array"), {
+		onGetAttributeValue: function(name, index, value) { 
+			return (this._arr[index] || {})[name]; 
+		}
+	}],
+
+	["vcl/ui/Panel", ("client"), { align: "client" }, [
+		["vcl/ui/Tabs", { classes: "bottom", align: "bottom" }, [
+		["vcl/ui/Tab", { text: "Table", control: "list"}],
+		["vcl/ui/Tab", { text: "Graphs", selected: true, control: "renderer"}]
+	]],
+		["vcl/ui/List", ("list"), { 
+			align: "client", autoColumns: true, visible: false, 
+			css: "background-color: white; min-width:100%;", 
+			source: "array",
+			onDblClick: function() {
+				this.print(this.getSelection(true));	
+			},
+			// onColumnGetValue: function(column, value, row, source) {
+			// 	value = this._source._arr[row][column._attribute];
+			// 	if(column.getIndex() === 0) {
+			// 		return row + " - " + value;
+			// 	}
+			// 	return value;
+			// }
+		}],
+		
+		[["devtools/Renderer<gds>"], ("renderer"), { visible: false }]
+	]]
+]];
