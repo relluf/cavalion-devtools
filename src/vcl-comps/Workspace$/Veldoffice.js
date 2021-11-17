b60356f1e5536ad72519df618972695e4ea9d60b
Ralph Kazemier <ralph.kazemier@gmail.com>
2021-11-17 10:18:01 -0600

    updating

--- src/vcl-comps/Workspace$/Veldoffice.js
+++ src/vcl-comps/Workspace$/Veldoffice.js
@@ -2,13 +2,14 @@ $([], {
 	vars: {
 		"#navigator favorites": [
 			"Workspaces/veldapps.com/Veldoffice/Veldoffice-glassfish/veldoffice2-front-end",
+			"Workspaces/veldapps.com/Veldoffice/veldoffice-vcl-comps",
 			"Workspaces/veldapps.com/Veldoffice/veldoffice-gebruik-vcl",
 			"Workspaces/veldapps.com/Veldoffice/veldoffice-geografie-vcl",
 			// "Workspaces/veldapps.com/Veldoffice/veldoffice-lib-vcl-comps",
 			"Workspaces/veldapps.com/Veldoffice/veldoffice-rapportage-vcl",
-			"Workspaces/veldapps.com/Veldoffice/veldoffice-rapportage-scripts",
+			// "Workspaces/veldapps.com/Veldoffice/veldoffice-rapportage-scripts",
 			"Workspaces/veldapps.com/Veldoffice/veldoffice-js",
-			"Library/cavalion-blocks/veldapps;veldapps-blocks"
+			// "Library/cavalion-blocks/veldapps;veldapps-blocks"
 		]
 	},
 	handlers: {
@@ -17,19 +18,17 @@ $([], {
 			Blocks.DEFAULT_NAMESPACES['vcl-veldoffice'] = "vcl-veldoffice";
 			Blocks.DEFAULT_NAMESPACES.veldoffice = "vcl-veldoffice";
 			
-			var keys = require("vcl/Component").getKeysByUri;
-			if(keys(this._uri).specializer_classes.length > 0) {
-				return;
-			}
-			
-			
 	/*- TODO this should flow back to devtools/Workspace - double click the corresponding tab to expand/collapse sub tabs. The idea is that the hotkeys activate a workspace (Cmd+1..9 remain fixed to address/focus an area (code/vcl/blocks/veldapps) and then another key could be pressed (ie. rapidly) to select a sub-tab (eg. Cmd+1, 3)*/
+			// var keys = require("vcl/Component").getKeysByUri;
+			// if(keys(this._uri).specializer_classes.length > 0) {
+			// 	return;
+			// }
 
-			var this_index = this.up("vcl/ui/Tab").getIndex(), tab;
+			// var this_index = this.up("vcl/ui/Tab").getIndex(), tab;
 			// this.udr("#workspace-needed").execute({workspace:{name: "Veldoffice/rapportage", selected: false}}).setIndex(this_index + 1);
 			// this.udr("#workspace-needed").execute({workspace:{name: "Veldoffice/geografie", selected: false}}).setIndex(this_index + 2);
-			this.udr("#workspace-needed").execute({workspace:{name: "Veldoffice/gebruik", selected: false}}).setIndex(this_index + 1);
-			this.udr("#workspace-needed").execute({workspace:{name: "Veldoffice/beheer", selected: false}}).setIndex(this_index + 2);
+			// this.udr("#workspace-needed").execute({workspace:{name: "Veldoffice/gebruik", selected: false}}).setIndex(this_index + 1);
+			// this.udr("#workspace-needed").execute({workspace:{name: "Veldoffice/beheer", selected: false}}).setIndex(this_index + 2);
 		}
 	}
 }, []);
