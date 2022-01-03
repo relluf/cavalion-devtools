0bb378803082040aae039abfad221e129fe04265
Ralph Kazemier <ralph.kazemier@gmail.com>
2022-01-03 15:12:28 -0600

    introducing var eval-context

--- src/vcl-comps/Console.js
+++ src/vcl-comps/Console.js
@@ -9,17 +9,28 @@ var Resources = require("devtools/Resources");
             var scope = this.scope();
             var app = this.app(), me = this;
             var ws = this.up(":root");
+            var ed = ws ? ws.qsa("devtools/Editor<>:root:visible").pop() : null;
             var ace = ws && ws.qsa("devtools/Editor<>:root:visible").map(_ => _.down("#ace")).pop();
+            
             // var ace = ws && ws.down("devtools/Editor<>:root < vcl/ui/Ace:visible");
-            var ed = ace ? ace.getEditor() : null;
             // var paths = window.require.s.contexts._.config.paths;
             
             var pr = this.print.bind(this);
-            var open = (uri, opts) => me.bubble("openform", js.mixIn(js.mixIn(opts || {}), {uri: uri}));
-            var ls = (path) => Resources.list(path || "/");
+            // var open = (uri, opts) => me.bubble("openform", js.mixIn(js.mixIn(opts || {}), {uri: uri}));
+            // var ls = (path) => Resources.list(path || "/");
+            
+            var ctx = this.vars(["eval-context", 0, this.vars("eval-context", Object.create(null, {
+            	root: { get() { 
+		            // var ace = ws.qsa("devtools/Editor<>:root:visible").map(_ => _.down("#ace")).pop();
+            		// var host = ace.ud("#host") || ace.getOwner();
+		            var ed = ws ? ws.qsa("devtools/Editor<>:root:visible").pop() : null;
+            		return ed && ed.vars("root"); 
+            	}  },
+            	open: { get() { return (uri, opts) => this.bubble("openform", js.mixIn(js.mixIn(opts || {}), {uri: uri})); } },
+            	ls: { get() { return (path) => Resources.list(path || "/"); } }
+            }))]);
 
-            // return window["ev"+(pr?"al":"")].apply(this, [expr]); // don't want no warnings (just calling eval)
-            return eval(expr);
+            with(ctx) { return eval(expr); }
         }
 	
 	}]
