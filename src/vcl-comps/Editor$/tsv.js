3975b869bfdc9db3bd7286af849264fb26703939
Ralph Kazemier <ralph.kazemier@gmail.com>
2022-01-25 03:09:11 -0600

    fixing delimiter detection

--- src/vcl-comps/Editor$/tsv.js
+++ src/vcl-comps/Editor$/tsv.js
@@ -1,1 +1,31 @@
-$(["devtools/Editor<csv>"]);
+"use js";
+
+[["devtools/Editor<csv>"], [
+
+	[("#render"), {
+		vars: {
+			options: {
+				delimiter: "\t",	// auto-detect
+				// newline: "",	// auto-detect
+				quoteChar: '"',
+				// escapeChar: '"',
+				header: true,
+				// dynamicTyping: false,
+				// preview: 0,
+				// encoding: "",
+				// worker: false,
+				// comments: false,
+				// step: undefined,
+				// complete: undefined,
+				// error: undefined,
+				// download: false,
+				skipEmptyLines: true,
+				// chunk: undefined,
+				// fastMode: undefined,
+				// beforeFirstChunk: undefined,
+				// withCredentials: undefined
+			}
+		}
+	}]
+	
+]];
