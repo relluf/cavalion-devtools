4ed80016dabdd74e5d160ad0515aabf255589698
Ralph Kazemier <ralph.kazemier@gmail.com>
2022-04-24 12:02:53 -0500

    enhancing previewing SVG graphic, now scrollable

--- src/vcl-comps/Editor$/svg.js
+++ src/vcl-comps/Editor$/svg.js
@@ -1,19 +1,21 @@
-$(["devtools/Editor<xml>"], [
-    $i("render", {
+[["devtools/Editor<xml>"], [
+    [("#render"), {
     	onExecute: function() {
         	var scope = this.scope();
 			scope.preview.getNode().innerHTML = scope.ace.getValue();
             return this.inherited(arguments);
     	}
-    }),
-	$i("console", {
-		visible: false}),
-    $i("output", [
-	    $("vcl/ui/Panel", "preview_wrapper", { align: "client" }, [
-		    $("vcl/ui/Panel", "preview", { align: "client", css: "text-align: center; padding-top: 10%;" })
-	    ])
-    ]),
-    $i("tabs", [
-    	$("vcl/ui/Tab", { text: locale("Preview"), control: "preview", selected: true }),
-    ])
-]);
+    }],
+	[("#console"), {
+		align: "top", autoSize:"height",
+		visible: false
+	}],
+    [("#output"), [
+	    ["vcl/ui/Panel", "preview_wrapper", { align: "client" }, [
+		    ["vcl/ui/Panel", "preview", { align: "client", css: "text-align: center; padding-top: 10%;" }]
+	    ]]
+    ]],
+    [("#tabs"), [
+    	["vcl/ui/Tab", { text: locale("Preview"), control: "preview", selected: true }]
+    ]],
+]];
