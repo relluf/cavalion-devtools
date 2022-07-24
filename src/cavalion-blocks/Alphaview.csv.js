daf0f4565f3bc01f2b9a6396a9cfb408086df0ff
Ralph Kazemier <ralph.kazemier@gmail.com>
2022-07-24 19:31:24 +0200

    enhances CSV generation, now conserving column headers and order

--- src/cavalion-blocks/Alphaview.csv.js
+++ src/cavalion-blocks/Alphaview.csv.js
@@ -8,7 +8,7 @@
 				selection = this.ud("#array")[filtered ? '_arr' : '_array'];
 			}
 
-var nameOf = (_) => _ instanceof Array ? js.sf("%d items", _.length) : js.nameOf(_);
+var valueOf = (value, column) => value instanceof Array ? value.length : js.nameOf(value);
 
 			var ws = this.up("devtools/Workspace<>:root");
 			var tab = ws.down("#editor-needed").execute({
@@ -16,14 +16,23 @@ var nameOf = (_) => _ instanceof Array ? js.sf("%d items", _.length) : js.nameOf
 				selected: true
 			});
 			
-			var header = [];
-			selection.forEach(_ => Object.keys(_).forEach(key => !header.includes(key) ? header.push(key) : []));
+			/* build the header based upon Object.keys(selection) */
+			var columnByH = (h) => list._columns.filter(c => c._attribute === h)[0];
+			var columnsOf = (header) => header.map(h => columnByH(h).getContent());
 			
+			var header = [], list = this.ud("#list");
+			selection.forEach(_ => Object.keys(_).forEach(key => !header.includes(key) ? header.push(key) : []));
+			header = header.sort((h1, h2) => {
+				var c1 = columnByH(h1).getIndex();
+				var c2 = columnByH(h2).getIndex();
+				return c1 === c2 ? 0 : c1 < c2 ? -1 : 1;
+			});
+
 			// this.print("remove", header.splice(0, 1)); // ? (1*)
 			
-			selection = selection.map(_ => js.sf("\"%s\"", header.map(h => _[h] !== undefined ? nameOf(_[h]) : "").join("\",\"")));
+			selection = selection.map(_ => js.sf("\"%s\"", header.map(h => _[h] !== undefined ? valueOf(_[h], columnByH(h)) : "").join("\",\"")));
 			tab.once("resource-loaded", function() {
-				tab.down("#ace").setValue([header].concat(selection).join("\n"));
+				tab.down("#ace").setValue([columnsOf(header)].concat(selection).join("\n"));
 			});
 		}
 	}],
