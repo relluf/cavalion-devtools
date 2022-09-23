539bc16ce6ad258642bc59b4841fe41947ecd084
Ralph Kazemier <ralph.kazemier@gmail.com>
2022-09-23 14:13:14 -0500

    cleans up some messy code

--- src/vcl-comps/Console.js
+++ src/vcl-comps/Console.js
@@ -12,26 +12,24 @@ var Resources = require("devtools/Resources");
             var ed = ws ? ws.qsa("devtools/Editor<>:root:visible").pop() : null;
             var ace = ws && ws.qsa("devtools/Editor<>:root:visible").map(_ => _.down("#ace")).pop();
             
-            // var ace = ws && ws.down("devtools/Editor<>:root < vcl/ui/Ace:visible");
-            // var paths = window.require.s.contexts._.config.paths;
-            
             var pr = this.print.bind(this);
-            // var open = (uri, opts) => me.bubble("openform", js.mixIn(js.mixIn(opts || {}), {uri: uri}));
-            // var ls = (path) => Resources.list(path || "/");
-            
+
             var ctx = this.vars(["eval-context", 0, this.vars("eval-context", Object.create(null, {
-            	root: { get() { 
-		            // var ace = ws.qsa("devtools/Editor<>:root:visible").map(_ => _.down("#ace")).pop();
-            		// var host = ace.ud("#host") || ace.getOwner();
-		            var ed = ws ? ws.qsa("devtools/Editor<>:root:visible").pop() : null;
-            		return ed && ed.vars("root"); 
-            	}  },
-            	open: { get() { return (uri, opts) => this.bubble("openform", js.mixIn(js.mixIn(opts || {}), {uri: uri})); } },
-            	ls: { get() { return (path) => Resources.list(path || "/"); } }
+            	root: { 
+            		get() {
+			            var ed = ws ? ws.qsa("devtools/Editor<>:root:visible").pop() : null;
+	            		return ed && ed.vars("root"); 
+	            	}  
+            	},
+            	ls: { 
+            		get() {
+            			return (path) => Resources.list(path || 
+            				js.up(ace.vars(["resource.uri"]))); 
+            		} 
+            	}
             }))]);
 
             with(ctx) { return eval(expr); }
         }
-	
 	}]
 ]];
