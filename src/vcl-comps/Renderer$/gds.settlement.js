ef04825a30dc5503f84df72847b4be42cfa36ace
Ralph Kazemier <ralph.kazemier@gmail.com>
2023-03-26 14:14:40 -0600

    first commit - starting development reporting triaxial tests from GDS-files

---
+++ src/vcl-comps/Renderer$/gds.settlement.js
@@ -0,0 +1,8 @@
+/*- TODO this should not be necessary CVLN-20230325-1 (probably duplicate) */
+"use js";
+
+[["devtools/Renderer<gds>", "devtools/Renderer<settlement>"], {}, []];
+
+/*- TODO CVLN-20230325-1 syntax request (now that you're at it) */
+
+// [["<gds>"], ["<settlement>"], {}, []];
---
+++ src/vcl-comps/Renderer$/locales/nl.js
@@ -0,0 +1,10 @@
+define((require) => ({
+
+	"devtools:Renderer:gds:": {
+		"Stage":						"Trap",
+		"Edit.trendlines":				"Lijnen muteren",
+		".label": "onder Pg:",
+		".label": "boven Pg:"
+	}
+
+}));
