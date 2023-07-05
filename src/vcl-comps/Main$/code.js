cfc0bac763f5c7ab8fd9ccf5e8bed19d8b883d23
relluf <ralph@relluf.nl>
2023-07-05 16:12:01 -0600

    delete obsolete code after refactoring Cavalion-code -> code

--- src/vcl-comps/Main$/code.js
+++ src/vcl-comps/Main$/code.js
@@ -1,1 +1,61 @@
-[["./Main<Cavalion-code>"]]; // just an alias
+"use blocks";
+
+["", {
+	
+	vars: {
+        "default-workspaces": [{
+            "name": "⌘1",
+            "selected": true,
+        	"vars": { "#navigator favorites": [ ".md;.md;File" ] },
+            "state": {
+	            "left-sidebar.visible": false,
+	            "editors": [{
+	                "selected": true,
+	                "resource": {
+	                    "uri": ".md",
+	                    "type": "File"
+	                }
+	            }]
+            }
+        },{
+            "name": "⌘2",
+            "state": {
+	            "editors": [{
+	                "selected": true,
+	                "resource": {
+	                    "uri": "README.md",
+	                    "type": "File"
+	                }
+	            }, {
+	                "resource": {
+	                    "uri": ".js",
+	                    "type": "File"
+	                }
+	            }, {
+	                "resource": {
+	                    "uri": ".blocks",
+	                    "type": "File"
+	                }
+	            }]
+            }
+        },{
+            name: "⌘3"
+        },{
+            name: "⌘4"
+        }]
+	}
+	
+}, [
+
+	// ["#workspaces-tabs", { _align: "top", _index: 0, _classes: "", zoom: 1 }]
+	
+]];
+
+
+
+
+
+
+
+
+// [["./Main<Cavalion-code>"]]; // just an alias
